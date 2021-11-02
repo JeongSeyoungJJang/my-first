2a02a2b6a682b55708b6391a7d87e591c2c4fd44
Jeong Seyoung <dfrsvg@hanmail.net>
2021-11-03 01:58:56 +0900

    1

---
+++ config/dev.config.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI : "mongodb+srv://dfrsvg:qwer1234@cluster0.1pivy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -2,16 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const bodyParser = require("body-parser")
+
+const config = require("./config/key.config")
+const { User } = require("./models/User.model");
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://dfrsvg:qwer1234@cluster0.1pivy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology : true,
 }).then(()=>console.log("mongo connected")).catch("error")
 
+
+// ! application/x-www-form-urlencoded 분석해서 가져오는
+app.use(bodyParser.urlencoded({extended:true}));
+// ! json타입 데이터를 분석
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
 
+
+app.post("/register",(req,res)=>{
+    // * 회원 가입할 떄 필요한 정보들을 클라이언트에서 가져오면
+    // * 그것들을 디비에 넣는다.
+
+    const user = new User(req.body);
+    user.save((err,userInfo)=> {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({success : true })
+    })
+})
 app.listen(PORT,() => {
     console.log("Server is running")
 })
